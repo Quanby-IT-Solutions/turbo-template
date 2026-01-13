8362bc7c85b4587080fd086647686aa0d6f630a7
Vencent Delute <iceiceicebell@gmail.com>
2026-01-13 11:40:40 +0800

    removed errors for personal info card

--- apps/web/app/(private)/patient/profile/components/cards/philhealth-info-card.tsx
+++ apps/web/app/(private)/patient/profile/components/cards/philhealth-info-card.tsx
@@ -25,8 +25,7 @@ import { Input } from "@/core/components/ui/input"
 import { Label } from "@/core/components/ui/label"
 import type { User } from "@/services/api/types"
 import { patientsApi } from "@/features/patients/api/patients-api"
-
-import type { PatientInfoType } from "../types"
+import type { PatientInfoType } from "@/features/patients/types/patients-types"
 
 type PhilHealthInfoCardProps = {
 	user: User | null

cb99057579df2427499c0de7ca6c9055a32fa3fb
SeanPalacay <jethroyacalap@gmail.com>
2026-04-12 03:24:41 +0800

    fix: reorder import statements in SiteLayout component

--- apps/web/app/(site)/layout.tsx
+++ apps/web/app/(site)/layout.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation"
 import Link from "next/link"
+import { redirect } from "next/navigation"
 
 import { getSession } from "@/services/better-auth/auth-server"
 
